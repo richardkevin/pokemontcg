cba044dadee58e88e225583f90d1c760c6bdb6b2
Richard Kevin <richard.correa@corp.globo.com>
2020-05-10 16:48:48 -0300

    wip

--- src/components/Attacks.js
+++ src/components/Attacks.js
@@ -1,33 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
 import energy from "../energy";
 
-export default class Attacks extends Component {
-  render() {
-    const { pokemon } = this.props;
+const Attacks = ({ pokemon }) => {
+  return (
+    <div className="attacks">
+      {pokemon.ability && (
+        <div>
+          <small className="ability__type">{pokemon.ability.type}</small>
+          <span className="ability__name">{pokemon.ability.name}</span>
+          <p className="attack__description">{pokemon.ability.text}</p>
+        </div>
+      )}
+      {pokemon.attacks.map((atk) => (
+        <div key={atk.name.replace(" ", "-")}>
+          <span className="attack__cost">
+            {atk.cost.map((type) => (
+              <img src={energy[type]} className="energy__cost" alt={type} />
+            ))}
+          </span>
+          <span className="attack__name">{atk.name}</span>
+          <span className="attack__damage right">{atk.damage}</span>
+          <p className="attack__description">{atk.text}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-    return (
-      <div className="attacks">
-        {pokemon.ability && (
-          <div>
-            <small className="ability__type">{pokemon.ability.type}</small>
-            <span className="ability__name">{pokemon.ability.name}</span>
-            <p className="attack__description">{pokemon.ability.text}</p>
-          </div>
-        )}
-        {pokemon.attacks.map((atk) => (
-          <div key={atk.name.replace(" ", "-")}>
-            <span className="attack__cost">
-              {atk.cost.map((type) => (
-                <img src={energy[type]} className="energy__cost" alt={type} />
-              ))}
-            </span>
-            <span className="attack__name">{atk.name}</span>
-            <span className="attack__damage right">{atk.damage}</span>
-            <p className="attack__description">{atk.text}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+export default Attacks;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 
 import energy from "../energy";
 import charmel from "../charmeleon.png";
 
-export default class Header extends Component {
-  render() {
-    const { pokemon } = this.props;
+const Header = ({ pokemon }) => {
+  return (
+    <div className="card-header">
+      {pokemon.evolvesFrom && (
+        <img src={charmel} className="evolves-from" alt="evolves-from" />
+      )}
+      <span className="pokemon__name">{pokemon.name}</span>
+      <span className="right">
+        <small>hp</small>
+        {pokemon.hp}
+        <img
+          src={energy[pokemon.types[0]]}
+          className="pokemon__type"
+          alt={pokemon.types[0]}
+        />
+      </span>
+    </div>
+  );
+};
 
-    return (
-      <div className="card-header">
-        {pokemon.evolvesFrom && (
-          <img src={charmel} className="evolves-from" alt="evolves-from" />
-        )}
-        <span className="pokemon__name">{pokemon.name}</span>
-        <span className="right">
-          <small>hp</small>
-          {pokemon.hp}
-          <img
-            src={energy[pokemon.types[0]]}
-            className="pokemon__type"
-            alt={pokemon.types[0]}
-          />
-        </span>
-      </div>
-    );
-  }
-}
+export default Header;
