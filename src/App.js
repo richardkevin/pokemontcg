f4320496f40e9fca2048668bd98c32a15a76a2b0
Richard Kevin <richard.correa@corp.globo.com>
2020-05-10 18:57:44 -0300

    Initialize versus layout

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 
 import Card from "./components/Card";
 import charizard from "./charizard.json";
+import gyarados from "./gyarados.json";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <Card pokemon={charizard} />;
+    return (
+      <div style={{display: "flex", justifyContent: "space-between"}}>
+        <Card pokemon={charizard} />
+        <Card pokemon={gyarados} />
+      </div>
+    );
   }
 }
 
