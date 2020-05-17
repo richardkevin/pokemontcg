9fde3a0ed73c2c87453ace7cb8dc61af127e1a40
Richard Kevin <richard.correa@corp.globo.com>
2020-05-17 04:40:18 -0300

    fetch new pokemon when faints

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
 
 import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 import reducers from "./reducers";
 import "./index.css";
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
